a267d57da7d42b5432077e303d961e5ef908e95c
HiroGori <hiro630705@gmail.com>
2021-03-11 15:57:06 +0900

    Added delete method in PostsController.

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -50,4 +50,14 @@ class PostsController extends Controller
         $post->fill($params)->save();
         return redirect()->route('posts.show', ['post' => $post]);
     }
+
+    public function destroy($post_id)
+    {
+        $post = Post::findOrFail($post_id);
+        \DB::transaction(function () use ($post) {
+            $post->comments()->delete();
+            $post->delete();
+        });
+        return redirect()->route('top');
+    }
 }
