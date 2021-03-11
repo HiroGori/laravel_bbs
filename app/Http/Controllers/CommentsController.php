fc11b0f3c1b48fc9d182e857c130b45afbe71ef0
HiroGori <hiro630705@gmail.com>
2021-03-11 16:05:38 +0900

    Modified typographical errors in CommentsController.

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -3,18 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App/Post;
+use App\Post;
 
 class CommentsController extends Controller
 {
     public function store(Request $request)
     {
-        $params = $request->validate({
+        $params = $request->validate([
             'post_id' => 'required|exists:posts,id',
             'body' => 'required|max:2000'
         ]);
         $post = post::findOrFail($params['post_id']);
-        %post->comments()->create($params);
+        $post->comments()->create($params);
         return redirect()->route('posts.show', ['post' => $post]);
     }
 }
